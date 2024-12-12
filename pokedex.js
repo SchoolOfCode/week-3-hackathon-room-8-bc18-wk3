d2d3f0ea65190ae184771f02576c48cb0041ca76
olliegwb <184382533+olliegwb@users.noreply.github.com>
2024-12-12 17:24:19 +0000

    added search bar functionality

--- pokedex.js
+++ pokedex.js
@@ -33,3 +33,32 @@ async function getPokemon() {
 function upperCaseFirstLetter(word) {
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
+
+document.getElementById("search").addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    const query = event.target.value.toLowerCase();
+    console.log(query);
+    searchPokemon(query);
+    event.target.value = "";
+  }
+})
+
+async function searchPokemon(query) {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
+  const json = await response.json();
+  console.log(json);
+  const img = document.getElementById("pokeball");
+  img.src = json.sprites.front_default;
+  const pokeNameEl = document.getElementById("pokemon");
+  let pokeName = upperCaseFirstLetter(json.name);
+  pokeNameEl.textContent = pokeName;
+  pokeType.innerHTML = "";
+  for (i = 0; i < json.types.length; i++) {
+    const newType = document.createElement("p");
+    newType.id = "type";
+    newType.textContent = `Type: ${upperCaseFirstLetter(
+      json.types[i].type.name
+    )}`;
+    pokeType.append(newType);
+}
+}
