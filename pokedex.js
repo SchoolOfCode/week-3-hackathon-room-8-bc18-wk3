ad3c771d531c90a09ddbfba85c6e4d7919bd46ba
fergus-g <fergus.gildea@utrack.com>
2024-12-13 12:27:55 +0000

    Removed searchPokemon API and reused getPokemon to allow us to search

--- pokedex.js
+++ pokedex.js
@@ -1,31 +1,55 @@
 // let randomPokemon = Math.floor(Math.random() * 1026) Commented out as unnecessary
 const pokeType = document.getElementById("type");
 const randomButtonHandler = document.getElementById("randoMon");
-randomButtonHandler.addEventListener("click", getPokemon);
-let pokeNumber = 0;
-
 const musicButtonHandler = document.getElementById("music");
-musicButtonHandler.addEventListener("click", play);
+const searchInput = document.getElementById("search");
+const searchButton = document.getElementById("pokeSearch");
 const audio = new Audio("pokemon-theme.mp3");
+let pokeNumber = 0;
+let query = "";
+let pokeCry = new Audio();
+
+searchInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    setSearchValueHandler();
+  }
+});
+
+searchButton.addEventListener("click", setSearchValueHandler);
+
+randomButtonHandler.addEventListener("click", getPokemonHandler);
+musicButtonHandler.addEventListener("click", play);
 
 function play() {
   audio.volume = 0.08;
   audio.paused ? audio.play() : audio.pause();
 }
 
-let pokeCry = new Audio();
-
 function playCry() {
   pokeCry.volume = 0.1;
   pokeCry.play();
 }
 
-async function getPokemon() {
+function getPokemonHandler() {
+  query = Math.floor(Math.random() * 1026);
+  getPokemon(query);
+}
+
+function setSearchValueHandler() {
+  query = searchInput.value.toLowerCase();
+  console.log(query);
+  getPokemon(query);
+  searchInput.value = "";
+}
+
+function upperCaseFirstLetter(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+async function getPokemon(query) {
   try {
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 1026)}`
-    );
-    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
+    if (!response.ok) alert(`${query} is not a Pokemon`);
     const json = await response.json();
     console.log(json);
     pokeCry = new Audio(json.cries.latest);
@@ -70,56 +94,40 @@ async function getPokemonFlavour() {
   }
 }
 
-function upperCaseFirstLetter(word) {
-  return word.charAt(0).toUpperCase() + word.slice(1);
-}
+// function getQuery() {
+//   const query = searchInput.value.toLowerCase();
+//   console.log(query);
+//   searchPokemon(query);
+//   searchInput.value = "";
+// }
 
-const searchInput = document.getElementById("search");
-const searchButton = document.getElementById("pokeSearch");
-
-searchInput.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-      getQuery();
-    }
-  });
-
-searchButton.addEventListener("click", getQuery);
-
-function getQuery() {
-  const query = searchInput.value.toLowerCase();
-  console.log(query);
-  searchPokemon(query);
-  searchInput.value = "";
-}
-
-
-async function searchPokemon(query) {
-  try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
-    if (!response.ok) {
-      alert(`${query} is not a Pokemon. Please try again.`)
-    }
-    const json = await response.json();
-    console.log(json);
-    pokeCry = new Audio(json.cries.latest);
-    playCry();
-    const img = document.getElementById("pokeball");
-    img.src = json.sprites.front_default;
-    const pokeNameEl = document.getElementById("pokemon");
-    let pokeName = upperCaseFirstLetter(json.name);
-    pokeNumber = json.id;
-    pokeNameEl.textContent = pokeName;
-    pokeType.innerHTML = "";
-    for (i = 0; i < json.types.length; i++) {
-      const newType = document.createElement("p");
-      newType.id = "type";
-      newType.textContent = `Type: ${upperCaseFirstLetter(
-        json.types[i].type.name
-      )}`;
-      pokeType.append(newType);
-    }
-    getPokemonFlavour();
-  } catch (error) {
-    console.error("Error fetching the Pokémon:", error);
-  }
-}
+// async function searchPokemon(query) {
+//   try {
+//     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
+//     if (!response.ok) {
+//       alert(`${query} is not a Pokemon. Please try again.`);
+//     }
+//     const json = await response.json();
+//     console.log(json);
+//     pokeCry = new Audio(json.cries.latest);
+//     playCry();
+//     const img = document.getElementById("pokeball");
+//     img.src = json.sprites.front_default;
+//     const pokeNameEl = document.getElementById("pokemon");
+//     let pokeName = upperCaseFirstLetter(json.name);
+//     pokeNumber = json.id;
+//     pokeNameEl.textContent = pokeName;
+//     pokeType.innerHTML = "";
+//     for (i = 0; i < json.types.length; i++) {
+//       const newType = document.createElement("p");
+//       newType.id = "type";
+//       newType.textContent = `Type: ${upperCaseFirstLetter(
+//         json.types[i].type.name
+//       )}`;
+//       pokeType.append(newType);
+//     }
+//     getPokemonFlavour();
+//   } catch (error) {
+//     console.error("Error fetching the Pokémon:", error);
+//   }
+// }
