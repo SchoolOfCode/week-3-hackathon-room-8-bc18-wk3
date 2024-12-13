517f31c328f63993c8ac43936ac2839468607a2c
olliegwb <184382533+olliegwb@users.noreply.github.com>
2024-12-13 10:27:58 +0000

    added error alert for incorrect input!

--- pokedex.js
+++ pokedex.js
@@ -59,40 +59,45 @@ const searchButton = document.getElementById("pokeSearch");
 
 searchInput.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
-      const query = searchInput.value.toLowerCase();
-      console.log(query);
-      searchPokemon(query);
-      event.target.value = "";
+      getQuery();
     }
   });
 
-  document.getElementById("pokeSearch").addEventListener("click", function () {
-    const query = searchInput.value.toLowerCase();
-    console.log(query);
-    searchPokemon(query);
-    searchInput.value = "";
-    }
-  );
+searchButton.addEventListener("click", getQuery);
+
+function getQuery() {
+  const query = searchInput.value.toLowerCase();
+  console.log(query);
+  searchPokemon(query);
+  searchInput.value = "";
+}
 
 
 async function searchPokemon(query) {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
-  const json = await response.json();
-  console.log(json);
-  const img = document.getElementById("pokeball");
-  img.src = json.sprites.front_default;
-  const pokeNameEl = document.getElementById("pokemon");
-  let pokeName = upperCaseFirstLetter(json.name);
-  pokeNumber = json.id;
-  pokeNameEl.textContent = pokeName;
-  pokeType.innerHTML = "";
-  for (i = 0; i < json.types.length; i++) {
-    const newType = document.createElement("p");
-    newType.id = "type";
-    newType.textContent = `Type: ${upperCaseFirstLetter(
-      json.types[i].type.name
-    )}`;
-    pokeType.append(newType);
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
+    if (!response.ok) {
+      alert(`${query} is not a Pokemon. Please try again.`)
+    }
+    const json = await response.json();
+    console.log(json);
+    const img = document.getElementById("pokeball");
+    img.src = json.sprites.front_default;
+    const pokeNameEl = document.getElementById("pokemon");
+    let pokeName = upperCaseFirstLetter(json.name);
+    pokeNumber = json.id;
+    pokeNameEl.textContent = pokeName;
+    pokeType.innerHTML = "";
+    for (i = 0; i < json.types.length; i++) {
+      const newType = document.createElement("p");
+      newType.id = "type";
+      newType.textContent = `Type: ${upperCaseFirstLetter(
+        json.types[i].type.name
+      )}`;
+      pokeType.append(newType);
+    }
+    getPokemonFlavour();
+  } catch (error) {
+    console.error("Error fetching the Pokémon:", error);
   }
-  getPokemonFlavour();
 }
